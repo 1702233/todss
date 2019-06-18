f7932d1d45a61c1d3bb902070a79d67801f0b70e
Gebruiker <jordje9@hotmail.com>
2019-06-18 14:25:00 +0200

    new arrangement aangepast

--- src/main/webapp/js/newarrangement.js
+++ src/main/webapp/js/newarrangement.js
@@ -1,11 +1,17 @@
+var minigameIDsInTable = [];
+
 function initPage() {
     getMinigames();
     addMinigameToArrangement();
     saveArrangement();
 
+
 }
 
 function getMinigames() {
+
+    clearDropdown();
+    console.log(minigameIDsInTable);
     var fetchoptionsGet = { method: 'GET' }
     var ingelogdeDocent = sessionStorage.getItem('docent');
 
@@ -26,12 +32,15 @@ function getMinigames() {
 
                 for (const minigame of myJson) { //maak een dropdown option voor elk object wat er in de myJson staat
 
+                    if (!minigameIDsInTable.includes(minigame.id)) {
+                        var option = document.createElement("option");
+                        var dropdownMinigames = document.getElementById("dropdownMinigames");
 
-                    var option = document.createElement("option");
-                    var dropdownMinigames = document.getElementById("dropdownMinigames");
-                    option.value = minigame.id;
-                    option.innerHTML = minigame.name + " - " + minigame.type;
-                    dropdownMinigames.appendChild(option);
+                        option.value = minigame.id;
+                        option.innerHTML = minigame.name + " - " + minigame.type;
+                        dropdownMinigames.appendChild(option);
+                    }
+
 
                 }
             }
@@ -46,7 +55,11 @@ function addMinigameToArrangement() {
     addButton.addEventListener("click", function () {
 
         if (dropdownMinigames.value != "") {
-            var minigameData = dropdownMinigames.innerHTML.split(" - ")
+            minigameIDsInTable.push(parseInt(dropdownMinigames.value));
+            
+
+            var value = dropdownMinigames[dropdownMinigames.selectedIndex].innerHTML;
+            var minigameData = value.split(" - ")
             var tr = document.createElement("tr");
             var td = document.createElement("td");
             var td2 = document.createElement("td");
@@ -54,13 +67,6 @@ function addMinigameToArrangement() {
             var td4 = document.createElement("td");
             var verwijderButton = document.createElement("button");
 
-            verwijderButton.innerHTML = "verwijder";
-            verwijderButton.addEventListener("click", function () {
-                var row = verwijderButton.parentNode.parentNode
-                row.parentNode.removeChild(row);
-            });
-            
-            
             td.innerHTML = dropdownMinigames.value;
             td2.innerHTML = minigameData[0];
             td3.innerHTML = minigameData[1];
@@ -71,7 +77,22 @@ function addMinigameToArrangement() {
             tr.appendChild(td4);
 
 
+
+
+            verwijderButton.innerHTML = "Verwijder";
+            verwijderButton.addEventListener("click", function () {
+                var row = verwijderButton.parentNode.parentNode
+                row.parentNode.removeChild(row);
+                console.log("td = " + td.innerHTML);
+                minigameIDsInTable.splice(minigameIDsInTable.indexOf(parseInt(td.innerHTML)), 1 );
+               
+                clearDropdown();
+                getMinigames();
+            });
+
+            getMinigames();
             arrangementTable.appendChild(tr);
+            dropdownMinigames.remove(dropdownMinigames.selectedIndex);
         }
 
     });
@@ -79,26 +100,26 @@ function addMinigameToArrangement() {
 
 function saveArrangement() {
 
-    
+
     var saveButton = document.getElementById("saveButton");
     saveButton.addEventListener("click", function () {
-        
+
         var minigameList = [];
         var table = document.getElementById("arrangementTable");
         var rows = table.getElementsByTagName('tr').length;
 
         for (var i = 1; i < rows; i++) {
-        minigameList.push(table.rows[i].cells[0].innerHTML);
+            minigameList.push(table.rows[i].cells[0].innerHTML);
         }
 
         var name = document.getElementById("name").value;
         var omschrijving = document.getElementById("omschrijving").value;
         var ingelogdeDocent = sessionStorage.getItem('docent');
-        var obj = { name: name, description: omschrijving, teacher: ingelogdeDocent, minigames: minigameList};
+        var obj = { name: name, description: omschrijving, teacher: ingelogdeDocent, minigames: minigameList };
         jsonString = JSON.stringify(obj);
 
-        
-        
+
+
 
         var fetchoptionsPost = {
             method: 'POST',
@@ -116,7 +137,7 @@ function saveArrangement() {
 
 
 
-        
+
 
         minigameList.forEach(function (element) {
             console.log("item");
@@ -126,6 +147,14 @@ function saveArrangement() {
 
 
 
+}
+
+function clearDropdown() {
+    var dropdownMinigames = document.getElementById("dropdownMinigames");
+    var length = dropdownMinigames.options.length;
+    for (i = 0; i < length; i++) {
+        dropdownMinigames.options[i] = null;
+    }
 }
 
 initPage();
